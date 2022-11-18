2df8330120f3fafee9f732808d9c7adf48156f7d
a-little-sweet <2768557792@qq.com>
2022-11-18 23:58:40 +0800

    Site updated: 2022-11-18 23:58:40

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [];
+var precacheConfig = [["/sw-register.js","08c2bad16f36cb5a623837807b923e04"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
